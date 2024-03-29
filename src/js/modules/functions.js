34bb5075e5c1af21c85e7f8c0a55533eb26abf73
Dmitrii Chizhov <dimachig@gmail.com>
2024-03-29 15:56:46 +0300

    add:adaptive galery 360/768/991/1200

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -69,135 +69,96 @@ export const typed = new Typed("#type-it", {
 
 /**Links toggle*/
 export const portfolioLinkToggle = () => {
-  $('.filter li').click(function() {
+  $('.portfolio-filter__nav li').click(function() {
     $(this).addClass('active').siblings().removeClass('active');
   });
 }
-
+// console.log(window.screen.availWidth);
 /**================================================================================*/
 /**-----------------------------PORTFOLIO GALLERY----------------------------------*/
 /**================================================================================*/
-const portfolioItem = document.getElementsByClassName('portfolio-item')
-const portfolioAll = document.getElementById("portfolio-all");
-const portfolioLayout = document.getElementById("portfolio-layout");
-const portfolioApps = document.getElementById("portfolio-apps");
-const pointer = document.getElementsByClassName('pointer')
-const portfolioFilter = document.getElementById('filter')
+
+  const portfolioItem = document.getElementsByClassName('portfolio-item')
+  const pointer = document.getElementsByClassName('pointer')
+  const portfolioFilter = document.getElementById('portfolio-filter')
 
 portfolioFilter.addEventListener('click', function (event) {
-  if(event.target === portfolioApps) {
-    portfolioItem[1].style.opacity = '0';
-    portfolioItem[1].style.transform = 'scale(0)';
-    portfolioItem[1].style.transition = '.5s';
-    //portfolioItem[1].style.display = 'none';
+  console.log(window.screen.availWidth);
+  if(event.target === pointer[2]) {
 
-    portfolioItem[2].style.opacity = '0';
-    portfolioItem[2].style.transform = 'scale(0)';
-    portfolioItem[2].style.transition = '.5s';
-    //portfolioItem[2].style.display = 'none';
-
-    portfolioItem[3].style.opacity = '0';
-    portfolioItem[3].style.transform = 'scale(0)';
-    portfolioItem[3].style.transition = '.5s';
-    //portfolioItem[3].style.display = 'none';
-
-    portfolioItem[5].style.opacity = '0';
-    portfolioItem[5].style.transform = 'scale(0)';
-    portfolioItem[5].style.transition = '.5s';
-    // portfolioItem[5].style.display = 'none';
-
-    portfolioItem[0].style.opacity = '1';
-    portfolioItem[0].style.transform = 'scale(1)';
-    portfolioItem[0].style.display = 'block';
-    portfolioItem[0].style.transition = '.5s';
-
-    portfolioItem[4].style.display = 'block';
-    portfolioItem[4].style.transform = 'scale(1)';
-    portfolioItem[4].style.opacity = '1';
-    portfolioItem[4].style.transition = '.5s';
+    for(let item of portfolioItem) {
+      if(item.classList.contains('apps')){
+        item.classList.remove('hide-item')
+        item.classList.add('show-item')
+      } else {
+        item.classList.add('hide-item')
+      }
+    }
     portfolioItem[4].style.top = '0';
 
-  }else if(event.target === portfolioLayout) {
-    portfolioItem[0].style.opacity = '0';
-    portfolioItem[0].style.transform = 'scale(0)';
-    portfolioItem[0].style.transition = '.5s';
-    //portfolioItem[0].style.display = 'none';
+  }else if(event.target === pointer[1]) {
+    for(let item of portfolioItem) {
+      if(item.classList.contains('layout')){
+        item.classList.add('show-item')
+        item.classList.remove('hide-item')
+      } else {
+        item.classList.add('hide-item')
+      }
+    }
 
-    portfolioItem[4].style.opacity = '0';
-    portfolioItem[4].style.transform = 'scale(0)';
-    portfolioItem[4].style.transition = '.5s';
-    //portfolioItem[4].style.display = 'none';
-
-    portfolioItem[1].style.display = 'block';
-    portfolioItem[1].style.opacity = '1';
-    portfolioItem[1].style.transform = 'scale(1)';
-    portfolioItem[1].style.transition = '.5s';
     portfolioItem[1].style.top = '0';
     portfolioItem[1].style.left = '0';
 
-    portfolioItem[2].style.display = 'block';
-    portfolioItem[2].style.opacity = '1';
-    portfolioItem[2].style.transform = 'scale(1)';
-    portfolioItem[2].style.transition = '.5s';
     portfolioItem[2].style.top = '0';
     portfolioItem[2].style.left = '33.3319%';
 
-    portfolioItem[3].style.display = 'block';
-    portfolioItem[3].style.opacity = '1';
-    portfolioItem[3].style.transform = 'scale(1)';
-    portfolioItem[3].style.transition = '.5s';
     portfolioItem[3].style.top = '0';
     portfolioItem[3].style.left = '66.6638%';
 
-    portfolioItem[5].style.display = 'block';
-    portfolioItem[5].style.opacity = '1';
-    portfolioItem[5].style.transform = 'scale(1)';
-    portfolioItem[5].style.transition = '.5s';
     portfolioItem[5].style.top = '28%';
     portfolioItem[5].style.left = '0';
 
-  }else if(event.target === portfolioAll) {
-    portfolioItem[0].style.opacity = '1';
-    portfolioItem[0].style.transform = 'scale(1)';
-    portfolioItem[0].style.transition = '.5s';
-    portfolioItem[0].style.display = 'block';
+  }else if(event.target === pointer[0]) {
+    for(let item of portfolioItem) {
+      item.classList.remove('hide-item')
+    }
     portfolioItem[0].style.top = '0';
     portfolioItem[0].style.left = '0';
 
-    portfolioItem[1].style.display = 'block';
-    portfolioItem[1].style.opacity = '1';
-    portfolioItem[1].style.transform = 'scale(1)';
-    portfolioItem[1].style.transition = '.5s';
     portfolioItem[1].style.top = '0';
     portfolioItem[1].style.left = '33.3319%';
 
-    portfolioItem[2].style.display = 'block';
-    portfolioItem[2].style.opacity = '1';
-    portfolioItem[2].style.transform = 'scale(1)';
-    portfolioItem[2].style.transition = '.5s';
     portfolioItem[2].style.top = '0';
     portfolioItem[2].style.left = '66.3338%';
 
-    portfolioItem[3].style.display = 'block';
-    portfolioItem[3].style.opacity = '1';
-    portfolioItem[3].style.transform = 'scale(1)';
-    portfolioItem[3].style.transition = '.5s';
     portfolioItem[3].style.top = '38%';
     portfolioItem[3].style.left = '0';
 
-    portfolioItem[4].style.display = 'block';
-    portfolioItem[4].style.opacity = '1';
-    portfolioItem[4].style.transform = 'scale(1)';
-    portfolioItem[4].style.transition = '.5s';
     portfolioItem[4].style.top = '28%';
     portfolioItem[4].style.left = '33.3319%';
 
-    portfolioItem[5].style.display = 'block';
-    portfolioItem[5].style.opacity = '1';
-    portfolioItem[5].style.transform = 'scale(1)';
-    portfolioItem[5].style.transition = '.5s';
     portfolioItem[5].style.top = '48%';
     portfolioItem[5].style.left = '66.3338%';
   }
-
-})
+});
+// window.addEventListener(`resize`, event => {
+//   let windowWidthSize = window.innerWidth;
+//   if(windowWidthSize > 320 < 576) {
+//     portfolioItem[0].style.top = '0'
+//     portfolioItem[0].style.left = '0'
+//     portfolioItem[0].style.width = '100%'
+//     portfolioItem[1].style.top = '40%'
+//     portfolioItem[1].style.width = '100%'
+//     portfolioItem[1].style.left = '0'
+//     portfolioItem[2].style.top = '60%'
+//     portfolioItem[2].style.left = '0'
+//     portfolioItem[2].style.width = '100%'
+//     portfolioItem[3].style.top = '30%'
+//     portfolioItem[3].style.left = '0'
+//     portfolioItem[4].style.top = '40%'
+//     portfolioItem[4].style.left = '0'
+//     portfolioItem[5].style.top = '40%'
+//     portfolioItem[5].style.left = '0'
+//   }
+//   console.log(windowWidthSize);
+// });
