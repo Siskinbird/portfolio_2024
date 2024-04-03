c91a9bf5b9bd4bcd79c5e9c8b7d26b1b76979204
Dmitrii Chizhov <dimachig@gmail.com>
2024-04-03 16:18:45 +0300

    add: new grid galery

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -78,83 +78,83 @@ export const portfolioLinkToggle = () => {
 /**-----------------------------PORTFOLIO GALLERY----------------------------------*/
 /**================================================================================*/
 
-  const portfolioItem = document.getElementsByClassName('portfolio-item')
-  const pointer = document.getElementsByClassName('pointer')
-  const portfolioFilter = document.getElementById('portfolio-filter')
+//   const portfolioItem = document.getElementsByClassName('portfolio-item')
+//   const pointer = document.getElementsByClassName('pointer')
+//   const portfolioFilter = document.getElementById('portfolio-filter')
 
-portfolioFilter.addEventListener('click', function (event) {
-  let screenSize  = window.screen.availWidth;
-  console.log(window.screen.availWidth);
-  if(event.target === pointer[2]) {
-    for(let item of portfolioItem) {
-      if(item.classList.contains('apps')){
-        item.classList.remove('hide-item')
-        item.classList.add('show-item')
-      } else {
-        item.classList.add('hide-item')
-      }
-    }
-    if (400 > screenSize < 576) {
-      console.log("poop");
-      //portfolioItem[4].style.left = '0';
-      portfolioItem[4].style.top = '20%';
-    }
-    if(screenSize < 400) {
-      portfolioItem[4].style.top = '17% !important';
-    }
-    if(screenSize < 365) {
-      portfolioItem[4].style.top = '14%';
-    }
-    // else {
-    //   portfolioItem[4].style.top = '0';
-    // }
+// portfolioFilter.addEventListener('click', function (event) {
+//   let screenSize  = window.screen.availWidth;
+//   console.log(window.screen.availWidth);
+//   if(event.target === pointer[2]) {
+//     for(let item of portfolioItem) {
+//       if(item.classList.contains('apps')){
+//         item.classList.remove('hide-item')
+//         item.classList.add('show-item')
+//       } else {
+//         item.classList.add('hide-item')
+//       }
+//     }
+//     if (400 > screenSize < 576) {
+//       console.log("poop");
+//       //portfolioItem[4].style.left = '0';
+//       portfolioItem[4].style.top = '20%';
+//     }
+//     if(screenSize < 400) {
+//       portfolioItem[4].style.top = '17% !important';
+//     }
+//     if(screenSize < 365) {
+//       portfolioItem[4].style.top = '14%';
+//     }
+//     // else {
+//     //   portfolioItem[4].style.top = '0';
+//     // }
 
 
-  }else if(event.target === pointer[1]) {
-    for(let item of portfolioItem) {
-      if(item.classList.contains('layout')){
-        item.classList.add('show-item')
-        item.classList.remove('hide-item')
-      } else {
-        item.classList.add('hide-item')
-      }
-    }
+//   }else if(event.target === pointer[1]) {
+//     for(let item of portfolioItem) {
+//       if(item.classList.contains('layout')){
+//         item.classList.add('show-item')
+//         item.classList.remove('hide-item')
+//       } else {
+//         item.classList.add('hide-item')
+//       }
+//     }
 
-    portfolioItem[1].style.top = '0';
-    portfolioItem[1].style.left = '0';
+//     portfolioItem[1].style.top = '0';
+//     portfolioItem[1].style.left = '0';
 
-    portfolioItem[2].style.top = '0';
-    portfolioItem[2].style.left = '33.3319%';
+//     portfolioItem[2].style.top = '0';
+//     portfolioItem[2].style.left = '33.3319%';
 
-    portfolioItem[3].style.top = '0';
-    portfolioItem[3].style.left = '66.6638%';
+//     portfolioItem[3].style.top = '0';
+//     portfolioItem[3].style.left = '66.6638%';
 
-    portfolioItem[5].style.top = '28%';
-    portfolioItem[5].style.left = '0';
+//     portfolioItem[5].style.top = '28%';
+//     portfolioItem[5].style.left = '0';
 
-  }else if(event.target === pointer[0]) {
-    for(let item of portfolioItem) {
-      item.classList.remove('hide-item')
-    }
-    portfolioItem[0].style.top = '0';
-    portfolioItem[0].style.left = '0';
+//   }else if(event.target === pointer[0]) {
+//     for(let item of portfolioItem) {
+//       item.classList.remove('hide-item')
+//     }
+//     portfolioItem[0].style.top = '0';
+//     portfolioItem[0].style.left = '0';
 
-    portfolioItem[1].style.top = '0';
-    portfolioItem[1].style.left = '33.3319%';
+//     portfolioItem[1].style.top = '0';
+//     portfolioItem[1].style.left = '33.3319%';
 
-    portfolioItem[2].style.top = '0';
-    portfolioItem[2].style.left = '66.3338%';
+//     portfolioItem[2].style.top = '0';
+//     portfolioItem[2].style.left = '66.3338%';
 
-    portfolioItem[3].style.top = '38%';
-    portfolioItem[3].style.left = '0';
+//     portfolioItem[3].style.top = '38%';
+//     portfolioItem[3].style.left = '0';
 
-    portfolioItem[4].style.top = '28%';
-    portfolioItem[4].style.left = '33.3319%';
+//     portfolioItem[4].style.top = '28%';
+//     portfolioItem[4].style.left = '33.3319%';
 
-    portfolioItem[5].style.top = '48%';
-    portfolioItem[5].style.left = '66.3338%';
-  }
-});
+//     portfolioItem[5].style.top = '48%';
+//     portfolioItem[5].style.left = '66.3338%';
+//   }
+// });
 // window.addEventListener(`resize`, event => {
 //   let windowWidthSize = window.innerWidth;
 //   if(windowWidthSize > 320 < 576) {
@@ -175,4 +175,65 @@ portfolioFilter.addEventListener('click', function (event) {
 //     portfolioItem[5].style.left = '0'
 //   }
 //   console.log(windowWidthSize);
+// });
+
+let items = document.getElementsByClassName('grid-item');
+
+document.getElementById('showAllItems').addEventListener('click', function(event) {
+  console.log(items)
+  for(let item in items) {
+    console.log(item)
+    item.target.classList.add('.show');
+  }
+})
+
+
+// document.getElementById('showAllItems').addEventListener('click', function(event) {
+//   event.preventDefault();
+//   items.forEach(function(item) {
+//     if(event.target.classList.contains('.hidden')) {
+//       item.classList.remove('hidden');
+//       item.classList.add('show');
+//     }
+//   });
+// });
+
+// document.getElementById('removeLastItems').addEventListener('click', function(event) {
+//   event.preventDefault();
+
+//   var items = document.querySelectorAll('.grid-item');
+//   var itemsToRemove = Array.prototype.slice.call(items, -2);
+
+//   itemsToRemove.forEach(function(item) {
+//       item.classList.add('hidden');
+//       setTimeout(function() {
+//           item.remove();
+//       }, 500);
+//   });
+// });
+
+// document.getElementById('removeFirstItems').addEventListener('click', function(event) {
+//   event.preventDefault();
+
+//   var items = document.querySelectorAll('.grid-item');
+//   var itemsToRemove = Array.prototype.slice.call(items, 0, 1);
+
+//   itemsToRemove.forEach(function(item) {
+//       item.classList.add('hidden');
+//       setTimeout(function() {
+//           item.remove();
+//       }, 500);
+//   });
+// });
+
+// document.getElementById('showAllItems').addEventListener('click', function(event) {
+//   event.preventDefault();
+
+//   var items = document.querySelectorAll('.grid-item.hidden');
+//   items.forEach(function(item) {
+//       item.classList.remove('hidden');
+//       setTimeout(function() {
+//           item.classList.add('show');
+//       }, 100);
+//   });
 // });
