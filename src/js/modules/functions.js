099f73e70e46e1c118919e9d959049166df7d7b5
Dmitrii Chizhov <dimachig@gmail.com>
2024-04-04 15:52:40 +0300

    fix: nihuya ne fix, home work

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -73,167 +73,34 @@ export const portfolioLinkToggle = () => {
     $(this).addClass('active').siblings().removeClass('active');
   });
 }
-// console.log(window.screen.availWidth);
-/**================================================================================*/
-/**-----------------------------PORTFOLIO GALLERY----------------------------------*/
-/**================================================================================*/
-
-//   const portfolioItem = document.getElementsByClassName('portfolio-item')
-//   const pointer = document.getElementsByClassName('pointer')
-//   const portfolioFilter = document.getElementById('portfolio-filter')
-
-// portfolioFilter.addEventListener('click', function (event) {
-//   let screenSize  = window.screen.availWidth;
-//   console.log(window.screen.availWidth);
-//   if(event.target === pointer[2]) {
-//     for(let item of portfolioItem) {
-//       if(item.classList.contains('apps')){
-//         item.classList.remove('hide-item')
-//         item.classList.add('show-item')
-//       } else {
-//         item.classList.add('hide-item')
-//       }
-//     }
-//     if (400 > screenSize < 576) {
-//       console.log("poop");
-//       //portfolioItem[4].style.left = '0';
-//       portfolioItem[4].style.top = '20%';
-//     }
-//     if(screenSize < 400) {
-//       portfolioItem[4].style.top = '17% !important';
-//     }
-//     if(screenSize < 365) {
-//       portfolioItem[4].style.top = '14%';
-//     }
-//     // else {
-//     //   portfolioItem[4].style.top = '0';
-//     // }
 
 
-//   }else if(event.target === pointer[1]) {
-//     for(let item of portfolioItem) {
-//       if(item.classList.contains('layout')){
-//         item.classList.add('show-item')
-//         item.classList.remove('hide-item')
-//       } else {
-//         item.classList.add('hide-item')
-//       }
-//     }
+let allItems = document.querySelectorAll('.grid-item');
+let links = document.querySelectorAll('.pointer');
 
-//     portfolioItem[1].style.top = '0';
-//     portfolioItem[1].style.left = '0';
-
-//     portfolioItem[2].style.top = '0';
-//     portfolioItem[2].style.left = '33.3319%';
-
-//     portfolioItem[3].style.top = '0';
-//     portfolioItem[3].style.left = '66.6638%';
-
-//     portfolioItem[5].style.top = '28%';
-//     portfolioItem[5].style.left = '0';
-
-//   }else if(event.target === pointer[0]) {
-//     for(let item of portfolioItem) {
-//       item.classList.remove('hide-item')
-//     }
-//     portfolioItem[0].style.top = '0';
-//     portfolioItem[0].style.left = '0';
-
-//     portfolioItem[1].style.top = '0';
-//     portfolioItem[1].style.left = '33.3319%';
-
-//     portfolioItem[2].style.top = '0';
-//     portfolioItem[2].style.left = '66.3338%';
-
-//     portfolioItem[3].style.top = '38%';
-//     portfolioItem[3].style.left = '0';
-
-//     portfolioItem[4].style.top = '28%';
-//     portfolioItem[4].style.left = '33.3319%';
-
-//     portfolioItem[5].style.top = '48%';
-//     portfolioItem[5].style.left = '66.3338%';
-//   }
-// });
-// window.addEventListener(`resize`, event => {
-//   let windowWidthSize = window.innerWidth;
-//   if(windowWidthSize > 320 < 576) {
-//     portfolioItem[0].style.top = '0'
-//     portfolioItem[0].style.left = '0'
-//     portfolioItem[0].style.width = '100%'
-//     portfolioItem[1].style.top = '40%'
-//     portfolioItem[1].style.width = '100%'
-//     portfolioItem[1].style.left = '0'
-//     portfolioItem[2].style.top = '60%'
-//     portfolioItem[2].style.left = '0'
-//     portfolioItem[2].style.width = '100%'
-//     portfolioItem[3].style.top = '30%'
-//     portfolioItem[3].style.left = '0'
-//     portfolioItem[4].style.top = '40%'
-//     portfolioItem[4].style.left = '0'
-//     portfolioItem[5].style.top = '40%'
-//     portfolioItem[5].style.left = '0'
-//   }
-//   console.log(windowWidthSize);
-// });
-
-let items = document.getElementsByClassName('grid-item');
-
-document.getElementById('showAllItems').addEventListener('click', function(event) {
-  console.log(items)
-  for(let item in items) {
-    console.log(item)
-    item.target.classList.add('.show');
+function layoutItems() {
+  let layout = [];
+allItems.forEach((item, index) => {
+  if(item.classList.contains('layout')) {
+    layout.push(item)
   }
+  console.log(layout)
+  return layout
 })
+}
 
 
-// document.getElementById('showAllItems').addEventListener('click', function(event) {
-//   event.preventDefault();
-//   items.forEach(function(item) {
-//     if(event.target.classList.contains('.hidden')) {
-//       item.classList.remove('hidden');
-//       item.classList.add('show');
-//     }
-//   });
-// });
 
-// document.getElementById('removeLastItems').addEventListener('click', function(event) {
-//   event.preventDefault();
 
-//   var items = document.querySelectorAll('.grid-item');
-//   var itemsToRemove = Array.prototype.slice.call(items, -2);
 
-//   itemsToRemove.forEach(function(item) {
-//       item.classList.add('hidden');
-//       setTimeout(function() {
-//           item.remove();
-//       }, 500);
-//   });
-// });
 
-// document.getElementById('removeFirstItems').addEventListener('click', function(event) {
-//   event.preventDefault();
+// const changeImage = (event) => {
+//   if(event.target.classList.contains('pointer')) {
+//     const gallery = event.target.dataset.galery;
+//     console.log(gallery)
+//     items.forEach((img, index) => console.log(img.src=`/img/portfolio/${gallery}/${index + 1}.jpg`))
+//   }
+// }
 
-//   var items = document.querySelectorAll('.grid-item');
-//   var itemsToRemove = Array.prototype.slice.call(items, 0, 1);
+links.forEach(link => {link.addEventListener('click',  layoutItems)})
 
-//   itemsToRemove.forEach(function(item) {
-//       item.classList.add('hidden');
-//       setTimeout(function() {
-//           item.remove();
-//       }, 500);
-//   });
-// });
-
-// document.getElementById('showAllItems').addEventListener('click', function(event) {
-//   event.preventDefault();
-
-//   var items = document.querySelectorAll('.grid-item.hidden');
-//   items.forEach(function(item) {
-//       item.classList.remove('hidden');
-//       setTimeout(function() {
-//           item.classList.add('show');
-//       }, 100);
-//   });
-// });
